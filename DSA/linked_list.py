549396aeaf7e961210cdb02110abec16b541977b
karthiga <karthigathangavel.2011@gmail.com>
2023-01-24 19:35:35 -0800

    insert at begining and end

--- DSA/linked_list.py
+++ DSA/linked_list.py
@@ -1,6 +1,8 @@
 #Linked List
     #create
     #traverse
+    #insert at begining
+    #insert at end
 
 #create linked list
 class HeadNode:
@@ -10,8 +12,21 @@ class HeadNode:
     def __init__(self):
         self.head= None 
 
+    def atBegining(self, node):
+        new_head = node
+        new_head.next = self.head
+        self.head = new_head
+    
+    def atEnd(self ,node):
+        if self.head.next is None: # if immediate next in 1st node is empty
+            self.head.next = node
+        at_end = self.head
+        while(at_end.next): # if immediate next node is not empty
+            at_end = at_end.next
+        at_end.next = node
+
     def display(self):
-        nodes = []
+        nodes = []       
         traverse_data = self.head
         while traverse_data is not None:
             nodes.append(traverse_data.data)
@@ -33,4 +48,8 @@ Node1 = Node('Orange') # pass orange to node
 initialize.head.next = Node1 # assing 2nd node to head node's next
 Node2 = Node('Grape')
 Node1.next = Node2 # assign 3rd node to 2nd node's next
+print(initialize.display())
+initialize.atBegining(Node('Pineapple'))
+print(initialize.display())
+initialize.atEnd(Node('Watermelon'))
 print(initialize.display())
