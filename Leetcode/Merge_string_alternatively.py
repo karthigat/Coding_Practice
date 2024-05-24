4ce9f3b8bcf601b164c45e0b5946575fa8b89b72
karthigat <karthigathangavel.2011@gmail.com>
2024-05-24 23:39:30 +0530

    prb2

---
+++ Leetcode/Merge_string_alternatively.py
@@ -0,0 +1,14 @@
+
+def merge_string(word1, word2):
+    result = []
+    i = 0
+    while i<len(word1) or i<len(word2):
+        if i<len(word1):
+            result.append(word1[i])
+        if i<len(word2):
+            result.append(word2[i])
+        i += 1
+    return ''.join(result) 
+word1 = "ab"
+word2 = "pqrs"
+print(merge_string(word1, word2))
